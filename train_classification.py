beaa2a04acb91bb4d0de1b4f77f95b98301788df
Data-drone <bpl.law@gmail.com>
2019-07-15 22:57:04 +0800

    added validation step

--- train_classification.py
+++ train_classification.py
@@ -92,6 +92,13 @@ if args.fp16: #or args.distributed:
         raise ImportError("Please install apex from https://www.github.com/nvidia/apex to run this script.")
 
 
+# item() is a recent addition, so this helps with backward compatibility.
+def to_python_float(t):
+    if hasattr(t, 'item'):
+        return t.item()
+    else:
+        return t[0]
+
 def accuracy(output, target, topk=(1,)):
     """Computes the precision@k for the specified values of k"""
     maxk = max(topk)
@@ -106,6 +113,93 @@ def accuracy(output, target, topk=(1,)):
         correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
         res.append(correct_k.mul_(100.0 / batch_size))
     return res
+
+def train(train_loader, model, criterion, optimizer, epoch):
+
+    model.train()
+    top1 = []
+    top5 = []
+    loss_list = []
+
+    for i, data in enumerate(train_loader):
+        input = data[0]["data"]
+        target = data[0]["label"].squeeze().cuda().long()
+        train_loader_len = int(train_loader._size / args.batch_size)
+
+        input_var = Variable(input)
+        target_var = Variable(target)
+
+        output = model(input_var)
+        loss = criterion(output, target_var)
+
+        reduced_loss = loss.data
+
+        prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
+
+        optimizer.zero_grad()
+
+        if args.fp16:
+            optimizer.backward(loss)
+        else:
+            loss.backward()
+        optimizer.step()
+
+        torch.cuda.synchronize()
+
+        # hold metrics
+        top1.append(to_python_float(prec1.cpu()))
+        top5.append(to_python_float(prec5.cpu()))
+        loss_list.append(to_python_float(reduced_loss.cpu()))
+        
+        if i % 20 == 0 and i > 1: 
+            stats = {"epoch": epoch, "loss": reduced_loss.cpu(), "Train Top-1": prec1.cpu(), 
+                        "Train Top-5": prec5.cpu()}
+            print('[{0} / {1}]'.format(i, train_loader_len))
+            print(stats)
+       
+    #wandb.log("epoch": epoch, "loss": )
+
+        
+
+
+def validate(val_loader, model, criterion, epoch):
+    
+    model.eval()
+    top1 = []
+    top5 = []
+    loss_list = []
+
+    for i, data in enumerate(val_loader):
+        input = data[0]["data"]
+        target = data[0]["label"].squeeze().cuda().long()
+        val_loader_len = int(val_loader._size / args.batch_size)
+
+        target = target.cuda(non_blocking=True)
+        input_var = Variable(input)
+        target_var = Variable(target)
+
+        # compute output
+        with torch.no_grad():
+            output = model(input_var)
+            loss = criterion(output, target_var)
+
+        # precision
+        prec1, prec5 = accuracy(output.data, target, topk=(1,5))
+
+        reduced_loss = loss.data
+
+        # hold metrics
+        top1.append(to_python_float(prec1.cpu()))
+        top5.append(to_python_float(prec5.cpu()))
+        loss_list.append(to_python_float(reduced_loss.cpu()))
+
+        if i % 20 == 0 and i > 1: 
+            stats = {"epoch": epoch, "loss": reduced_loss.cpu(), "Val Top-1": prec1.cpu(), 
+                        "Val Top-5": prec5.cpu()}
+            print('[{0} / {1}]'.format(i, val_loader_len))
+            print(stats)
+
+    
 
 def main():
     
@@ -181,43 +275,15 @@ def main():
     pipe.build()
     val_loader = DALIClassificationIterator(pipe, size=int(pipe.epoch_size("Reader") / args.world_size))
 
-    model.train()
+    
 
     for epoch in range(0, 10):
 
         # train loop
-        for i, data in enumerate(train_loader):
-            input = data[0]["data"]
-            target = data[0]["label"].squeeze().cuda().long()
-            train_loader_len = int(train_loader._size / args.batch_size)
-
-            input_var = Variable(input)
-            target_var = Variable(target)
-
-            output = model(input_var)
-            loss = criterion(output, target_var)
-
-            reduced_loss = loss.data
-
-            prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
-
-            optimizer.zero_grad()
-
-            if args.fp16:
-                optimizer.backward(loss)
-            else:
-                loss.backward()
-            optimizer.step()
-
-            torch.cuda.synchronize()
+        train(train_loader, model, criterion, optimizer, epoch)
+        validate(val_loader, model, criterion, epoch)
             
-            if i % 20 == 0 and i > 1: 
-                stats = {"epoch": epoch, "loss": reduced_loss.cpu(), "Train Top-1": prec1.cpu(), 
-                            "Train Top-5": prec5.cpu()}
-                print('[{0} / {1}]'.format(i, train_loader_len))
-                print(stats)
-            
-            #wandb.log({"epoch" epoch, "loss": loss.cpu(), "Train Top-1": prec1.cpu(), "Train Top-5": prec5.cpu()})
+        #wandb.log({"epoch" epoch, "loss": loss.cpu(), "Train Top-1": prec1.cpu(), "Train Top-5": prec5.cpu()})
         
         # do I need a validate loop here (how should we log losses?)
 
