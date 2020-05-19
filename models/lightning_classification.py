591269295abe6559f479c11e85f52ae9d08c65bf
Data-drone <bpl.law@gmail.com>
2020-05-19 21:43:28 +1000

    moved model config into the lightning_classification file rather than the outer file

--- models/lightning_classification.py
+++ models/lightning_classification.py
@@ -7,23 +7,59 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 import torchvision
 import torchvision.transforms as transforms
+import torchvision.models as models
+import models as local_models
+from models.layer_utils import Swish, Mish
+
+import optimisers as local_optimisers
 
 from pytorch_lightning import _logger as log
 from pytorch_lightning.core import LightningModule
+
+from torch.optim.lr_scheduler import CyclicLR
+
 ## lightning wrapper around cvision
 # Need to test
 
 
 class LightningModel(LightningModule):
 
-    def __init__(self, hparams, model, optimizer, scheduler, criterion):
+    def __init__(self, hparams, criterion):
         super().__init__()
 
+        model_names = sorted(name for name in models.__dict__
+                            if name.islower() and not name.startswith("__")
+                            and callable(models.__dict__[name]))
+
+        local_model_names = sorted(name for name in local_models.__dict__
+                            if name.islower() and not name.startswith("__")
+                            and callable(local_models.__dict__[name]))
+
+        valid_models = model_names + local_model_names
+
         self.hparams = hparams
-        self.model = model
-        self.optimizer = optimizer
-        self.scheduler = scheduler
+
+        if self.hparams.act_func == 'swish':
+            self.act_funct = Swish()
+        elif self.hparams.act_func == 'mish':
+            self.act_funct = Mish()
+        elif self.hparams.act_func == 'relu':
+            self.act_funct = nn.ReLU(inplace=True)
+
+        # initiate model
+        print("=> creating new model '{}'".format(self.hparams.model))
+        if self.hparams.model in model_names:
+            cv_model = models.__dict__[self.hparams.model](pretrained=False)
+        elif self.hparams.model in local_model_names:
+            cv_model = local_models.__dict__[self.hparams.model](pretrained=False,
+                                                        activation=self.act_funct)
+
+        if self.hparams.model == 'inception_v3':
+            cv_model.aux_logits=False
+
+        self.model = cv_model
         self.criterion = criterion
+
 
     def forward(self, x):
 
@@ -83,7 +119,34 @@ class LightningModel(LightningModule):
 
     def configure_optimizers(self):
 
-        return [self.optimizer], [self.scheduler]
+        # optimizer
+        if self.hparams.opt == 'sgd':
+            optimizer = torch.optim.SGD(self.model.parameters(), self.hparams.lr,
+                                    momentum=self.hparams.momentum,
+                                    weight_decay=self.hparams.weight_decay)
+
+        if self.hparams.opt == 'adam':
+            optimizer = torch.optim.Adam(self.model.parameters(), self.hparams.lr,
+                                        weight_decay=self.hparams.weight_decay)
+
+        if self.hparams.opt == 'adamw':
+            optimizer = torch.optim.AdamW(self.model.parameters(), self.hparams.lr,
+                                        weight_decay=self.hparams.weight_decay)
+            
+        if self.hparams.opt == 'radam':
+            optimizer = local_optimisers.RAdam(self.model.parameters(), self.hparams.lr,
+                                            betas=(0.9, 0.999), eps=1e-8,
+                                            weight_decay=self.hparams.weight_decay)
+
+        if self.hparams.opt == 'ranger':
+            optimizer = local_optimisers.Ranger(self.model.parameters(), self.hparams.lr,
+                                                weight_decay=self.hparams.weight_decay)
+
+        # scheduler
+        scheduler = CyclicLR(optimizer, 0.00001, self.hparams.lr)
+
+
+        return [optimizer], [scheduler]
 
     
     def prepare_data(self):
@@ -98,15 +161,15 @@ class LightningModel(LightningModule):
 
     def train_dataloader(self):
         log.info('Training data loader called.')
-        return DataLoader(self.train_data, batch_size=self.hparams.batch_size, num_workers=4)
+        return DataLoader(self.train_data, batch_size=self.hparams.batch_size, num_workers=self.hparams.nworkers)
 
     def val_dataloader(self):
         log.info('Validation data loader called.')
-        return DataLoader(self.test_data, batch_size=self.hparams.batch_size, num_workers=4)
+        return DataLoader(self.test_data, batch_size=self.hparams.batch_size, num_workers=self.hparams.nworkers)
 
     def test_dataloader(self):
         log.info('Test data loader called.')
-        return DataLoader(self.test_data, batch_size=self.hparams.batch_size, num_workers=4)
+        return DataLoader(self.test_data, batch_size=self.hparams.batch_size, num_workers=self.hparams.nworkers)
 
     @staticmethod
     def add_model_specific_args(parent_parser):  # pragma: no-cover
@@ -115,5 +178,23 @@ class LightningModel(LightningModule):
 
         parser.add_argument('--epochs', default=20, type=int)
         parser.add_argument('--batch_size', default=64, type=int)
+        parser.add_argument('--nworkers', default=4, type=int)
+
+        parser.add_argument('--act_func',
+                            type=str,
+                            default='relu',
+                            choices = ['relu', 'swish', 'mish'],
+                            help='enter a valid model name')
+
+        parser.add_argument('--model',
+                            type=str,
+                            default='resnet18',
+                            help='enter a valid model name')
+
+        parser.add_argument('--opt',
+                            type=str,
+                            default='sgd',
+                            choices=['sgd', 'adam', 'adamw', 'radam', 'ranger'])
+
 
         return parser
--- train_classification_lightning.py
+++ train_classification_lightning.py
@@ -11,28 +11,12 @@ import torch.nn as nn
 
 import pytorch_lightning as pl
 from models.lightning_classification import LightningModel
-import torchvision.models as models
-import models as local_models
-import optimisers as local_optimisers
 
 from lr_schedulers.onecyclelr import OneCycleLR
-from torch.optim.lr_scheduler import CyclicLR
-from models.layer_utils import Swish, Mish
 
 SEED = 2334
 torch.manual_seed(SEED)
 np.random.seed(SEED)
-
-
-model_names = sorted(name for name in models.__dict__
-                     if name.islower() and not name.startswith("__")
-                     and callable(models.__dict__[name]))
-
-local_model_names = sorted(name for name in local_models.__dict__
-                     if name.islower() and not name.startswith("__")
-                     and callable(local_models.__dict__[name]))
-
-valid_models = model_names + local_model_names
 
 
 def main(hparams):
@@ -45,56 +29,18 @@ def main(hparams):
     # ------------------------
 
     # activation function
-    if hparams.act_func == 'swish':
-        act_funct = Swish()
-    elif hparams.act_func == 'mish':
-        act_funct = Mish()
-    elif hparams.act_func == 'relu':
-        act_funct = nn.ReLU(inplace=True)
-
-
-    # initiate model
-    print("=> creating new model '{}'".format(hparams.model))
-    if hparams.model in model_names:
-        cv_model = models.__dict__[hparams.model](pretrained=False)
-    elif hparams.model in local_model_names:
-        cv_model = local_models.__dict__[hparams.model](pretrained=False,
-                                                    activation=act_funct)
-
-    if hparams.model == 'inception_v3':
-        model.aux_logits=False
-
-    # optimizer
-    if hparams.opt == 'sgd':
-        optimizer = torch.optim.SGD(cv_model.parameters(), hparams.lr,
-                                momentum=hparams.momentum,
-                                weight_decay=hparams.weight_decay)
-
-    if hparams.opt == 'adam':
-        optimizer = torch.optim.Adam(cv_model.parameters(), hparams.lr,
-                                    weight_decay=hparams.weight_decay)
-
-    if hparams.opt == 'adamw':
-        optimizer = torch.optim.AdamW(cv_model.parameters(), hparams.lr,
-                                    weight_decay=hparams.weight_decay)
-        
-    if hparams.opt == 'radam':
-        optimizer = local_optimisers.RAdam(cv_model.parameters(), hparams.lr,
-                                          betas=(0.9, 0.999), eps=1e-8,
-                                           weight_decay=hparams.weight_decay)
-
-    if hparams.opt == 'ranger':
-        optimizer = local_optimisers.Ranger(cv_model.parameters(), hparams.lr,
-                                            weight_decay=hparams.weight_decay)
-
-    # scheduler
-    scheduler = CyclicLR(optimizer, 0.00001, hparams.lr)
-
+    #if hparams.act_func == 'swish':
+    #    act_funct = Swish()
+    #elif hparams.act_func == 'mish':
+    #    act_funct = Mish()
+    #elif hparams.act_func == 'relu':
+    #    act_funct = nn.ReLU(inplace=True)
+    
     # criterion
     criterion = nn.CrossEntropyLoss()
 
     # send to lightning
-    model = LightningModel(hparams, cv_model, optimizer, scheduler, criterion)
+    model = LightningModel(hparams, criterion)
 
     # ------------------------
     # 2 INIT TRAINER
@@ -141,27 +87,6 @@ if __name__ == '__main__':
         help='if true uses 16 bit precision'
     )
 
-    parent_parser.add_argument(
-        '--model',
-        type=str,
-        default='resnet18',
-        help='enter a valid model name'
-    )
-
-    parent_parser.add_argument(
-        '--act_func',
-        type=str,
-        default='relu',
-        choices = ['relu', 'swish', 'mish'],
-        help='enter a valid model name'
-    )
-
-    parent_parser.add_argument(
-        '--opt',
-        type=str,
-        default='adam',
-        choices=['sgd', 'adam', 'adamw', 'radam', 'ranger']
-    )
 
     parent_parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
                     metavar='LR', help='Initial learning rate.  Will be scaled by <global batch size>/256: args.lr = args.lr*float(args.batch_size*args.world_size)/256.  A warmup schedule will also be applied over the first 5 epochs.')
