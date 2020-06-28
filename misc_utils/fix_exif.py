043e1c12faad1e585ac8d2e6c938ef3076a52039
Data-drone <bpl.law@gmail.com>
2020-06-28 18:44:06 +1000

    switched to parallel loop but hitting disk io bottlenecks

--- misc_utils/fix_exif.py
+++ misc_utils/fix_exif.py
@@ -4,12 +4,35 @@
 import glob
 import piexif
 
-nfiles = 0
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+
 dirs = ['../external_data/ImageNet/ILSVRC2012_img_train/**/*.JPEG',
         '../external_data/ImageNet/ILSVRC2012_img_val/**/*.JPEG']
 
+def clean_file(file_name):
+    piexif.remove(file_name)
+
+
 for folder in dirs:
+    file_list = glob.glob(folder, recursive=True)
+
+    pool = Pool(cpu_count()-1)
+    results = pool.map(clean_file, file_list)
+    pool.close()  # 'TERM'
+    pool.join() 
+
+    #print(len(file_list))
+    
+    #print(file_list[:2])
+
+
+
+    """
     for filename in glob.iglob(folder, recursive=True):
         nfiles = nfiles + 1
         print("About to process file %d, which is %s." % (nfiles,filename))
         piexif.remove(filename)
+
+    """
+    
