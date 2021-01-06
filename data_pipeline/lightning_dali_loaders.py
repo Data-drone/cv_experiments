345a59af1a88adeb4cfe71ffac9d35a36649800b
Data-drone <bpl.law@gmail.com>
2021-01-06 22:03:40 +1100

    got updated dali loader working

--- data_pipeline/lightning_dali_loaders.py
+++ data_pipeline/lightning_dali_loaders.py
@@ -1,29 +1,43 @@
 # Dali dataloaders for pytorch lightning
 from pytorch_lightning import LightningDataModule
 from data_pipeline.basic_pipeline import HybridTrainPipe, HybridValPipe
+from data_pipeline.lightning_pipeline import DaliTransformsTrainPipeline, DaliTransformsValPipeline
 import torch
 from nvidia.dali.plugin.pytorch import DALIClassificationIterator
 
 class PLDaliPipe(LightningDataModule):
-    def __init__(self, hparams, train_datadir, val_datadir):
+    def __init__(self, hparams, train_datadir, val_datadir, mean, std):
         super().__init__()
 
         self.hparams = hparams
         self.train_datadir = train_datadir
         self.val_datadir = val_datadir
+        self.dataset_mean = mean
+        self.dataset_std = std
 
     def setup(self, stage=None):
-        # batch_size=args.batch_size, num_threads=args.workers, device_id=args.local_rank, 
-        #                    data_dir=traindir, crop=crop_size, dali_cpu=False
+        # setup runs on all gpus all nodes at the start
+
         device_id = self.trainer.local_rank
         shard_id = self.trainer.global_rank
         num_shards = self.trainer.world_size
-        train_pipeline = HybridTrainPipe(batch_size=self.hparams.batch_size, num_threads=8, 
-                                            device_id=device_id, data_dir=self.train_datadir, crop=224, 
-                                            dali_cpu=False)
 
-        val_pipline = HybridValPipe(batch_size=self.hparams.batch_size, num_threads=8, 
-                                            device_id=device_id, data_dir=self.val_datadir, crop=224, size=256)
+        train_pipeline = DaliTransformsTrainPipeline(batch_size=self.hparams.batch_size, 
+                    device='gpu', data_dir=self.train_datadir, mean=self.dataset_mean, 
+                    std=self.dataset_std, device_id=device_id, 
+                    shard_id=shard_id, num_shards=num_shards, num_threads=4, seed=12+device_id) 
+
+        #train_pipeline = HybridTrainPipe(batch_size=self.hparams.batch_size, num_threads=8, 
+        #                                    device_id=device_id, data_dir=self.train_datadir, crop=224, 
+        #                                    dali_cpu=False)
+        
+        val_pipeline = DaliTransformsValPipeline(batch_size=self.hparams.batch_size, 
+                    device='gpu', data_dir=self.val_datadir, mean=self.dataset_mean, 
+                    std=self.dataset_std, device_id=device_id, 
+                    shard_id=shard_id, num_shards=num_shards, num_threads=4, seed=12+device_id)
+
+        #val_pipeline = HybridValPipe(batch_size=self.hparams.batch_size, num_threads=8, 
+        #                                    device_id=device_id, data_dir=self.val_datadir, crop=224, size=256)
 
         class LightningWrapper(DALIClassificationIterator):
             def __init__(self, *kargs, **kvargs):
@@ -40,7 +54,7 @@ class PLDaliPipe(LightningDataModule):
         self.train_loader = LightningWrapper(train_pipeline, reader_name="Reader", 
                                                 auto_reset=True)
 
-        self.val_loader = LightningWrapper(val_pipline, reader_name="Reader", 
+        self.val_loader = LightningWrapper(val_pipeline, reader_name="Reader", 
                                                 auto_reset=True)
 
     def train_dataloader(self):
--- data_pipeline/lightning_pipeline.py
+++ data_pipeline/lightning_pipeline.py
@@ -10,32 +10,34 @@ except ImportError:
 class DaliTransformsTrainPipeline(Pipeline):
     def __init__(self, batch_size, device, data_dir, mean, std, device_id=0, 
                     shard_id=0, num_shards=1, num_threads=4, seed=0):
-        super(DaliTransformsTrainPipeline, self).__init__(
-            batch_size, num_threads, device_id, seed)
+        super(DaliTransformsTrainPipeline, self).__init__(batch_size, num_threads, device_id, seed)
 
         # should we make this drive the device flags?
-        self.device = device
-
         self.reader = ops.FileReader(file_root=data_dir, shard_id=shard_id, 
                                         num_shards=num_shards, random_shuffle=True)
 
         self.decode = ops.ImageDecoder(device='mixed', output_type=types.RGB, memory_stats=True)
-        self.resize = ops.Resize(device='gpu', size=[200,300], interp_type=types.INTERP_TRIANGULAR)
-        self.centrecrop = ops.Crop(device='gpu', crop=[100,100])
-        self.randomcrop = ops.RandomResizedCrop(device='gpu', size=[80,80])
+        self.resize = ops.Resize(device=device, size=[200,300], interp_type=types.INTERP_TRIANGULAR)
+        self.centrecrop = ops.Crop(device=device, crop=[100,100])
+        self.randomcrop = ops.RandomResizedCrop(device=device, size=[80,80])
 
         self.hz_coin = ops.CoinFlip(probability=0.5)
-        self.horizontalflip = ops.Flip(device='gpu')
+        self.horizontalflip = ops.Flip(device=device)
 
         self.rotate_angle = ops.Uniform(range=[-90,90])
         self.rotate_coin = ops.CoinFlip(probability=0.5)
-        self.rotate = ops.Rotate(device='gpu', keep_size=True)
+        self.rotate = ops.Rotate(device=device, keep_size=True)
         
         self.vt_coin = ops.CoinFlip(probability=0.5)
-        self.verticalflip = ops.Flip(device='gpu', horizontal=0)
+        self.verticalflip = ops.Flip(device=device, horizontal=0)
 
-        self.normalize = ops.Normalize(device='gpu', dtype=types.FLOAT)#, mean=mean, stddev=std)
-        self.to_int64 = ops.Cast(dtype=types.INT64, device="gpu")
+        self.normalize = ops.CropMirrorNormalize(device=device, dtype=types.FLOAT, 
+                                                output_layout=types.NCHW) #,
+                                                #mean=mean*255, std=std*255)
+
+        #self.normalize = ops.Normalize(device=device, dtype=types.FLOAT)#, mean=mean, stddev=std)
+        self.to_int64 = ops.Cast(dtype=types.INT64, device=device)
+        #self.transpose_torch = ops.Transpose(device=device, perm=[0,1,2,3], output_layout=types.NCHW)
 
 
     def define_graph(self):
@@ -53,12 +55,12 @@ class DaliTransformsTrainPipeline(Pipeline):
         labels = self.to_int64(labels)
         
         images = self.decode(jpegs)
-        images = self.resize(images)
+        images = self.resize(images.gpu())
         images = self.centrecrop(images)
         images = self.randomcrop(images)
         images = self.horizontalflip(images, horizontal=hz_rng)
         # We multiple the uniform dist sample with the 0/1 from the coin flip
-        images = self.rotate(images, angle=rt_coin*rt_angle)
+        #images = self.rotate(images, angle=rt_coin*rt_angle)
         images = self.verticalflip(images, vertical=vr_rng)
         images = self.normalize(images)
         
@@ -73,27 +75,30 @@ class DaliTransformsValPipeline(Pipeline):
             batch_size, num_threads, device_id, seed)
 
         # should we make this drive the device flags?
-        self.device = device
-
         self.reader = ops.FileReader(file_root=data_dir, shard_id=shard_id, 
                                         num_shards=num_shards, random_shuffle=False)
+
         self.decode = ops.ImageDecoder(device='mixed', output_type=types.RGB, memory_stats=True)
-        self.resize = ops.Resize(device='gpu', size=[200,300], interp_type=types.INTERP_TRIANGULAR)
+        self.resize = ops.Resize(device=device, size=[200,300], interp_type=types.INTERP_TRIANGULAR)
 
-        self.normalize = ops.Normalize(device='gpu', dtype=types.FLOAT)#, mean=mean, stddev=std)
-        self.to_int64 = ops.Cast(dtype=types.INT64, device="gpu")
+        self.normalize = ops.CropMirrorNormalize(device=device, dtype=types.FLOAT, 
+                                                output_layout=types.NCHW) #,
+                                                #mean=mean*255, std=std*255)
 
+        #self.normalize = ops.Normalize(device=device, dtype=types.FLOAT)#, mean=mean, stddev=std)
+        self.to_int64 = ops.Cast(dtype=types.INT64, device=device)
+        
 
     def define_graph(self):
 
-        jpegs, labels = self.reader()
+        jpegs, labels = self.reader(name="Reader")
         
         labels = labels.gpu()
         # PyTorch expects labels as INT64
         labels = self.to_int64(labels)
         
         images = self.decode(jpegs)
-        images = self.resize(images)
+        images = self.resize(images.gpu())
         images = self.normalize(images)
         
         return (images, labels)
